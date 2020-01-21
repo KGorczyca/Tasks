128fb7c932a6c0124e44a492faf7a335fa0fd6d0
KGorczyca <kmgorczyca@wp.pl>
2020-01-21 12:09:07 +0100

    dodanie licznika czasu

--- GraLosowa.py
+++ GraLosowa.py
@@ -3,16 +3,21 @@
 
 
 import random
+import time
+
 my_number = random.randint(0,100)
 guess = -1
 trials = 0
 
 print("Guess my number from 0 to 100")
 while guess != my_number:
+    start_time = time.time()
     guess = int(input())
     trials+=1
     if guess == my_number:
-        print("Congratulations! You guessed the number for the",trials,"time")
+        end_time = time.time()
+        total_time = round(end_time-start_time,2)
+        print("Congratulations! You guessed the number for the",trials,"time, and it takes:", total_time)
     elif guess > my_number:
         print("Sorry - my number is smaller than your guess,try again!")
     else:
